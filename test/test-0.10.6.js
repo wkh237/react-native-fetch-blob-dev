9fbe1b25411e5541a94d2b5571ee3d426dd1a883
Ben Hsieh <xeiyan@gmail.com>
2017-05-14 14:42:17 +0800

    Add test case for 0.10.6

---
+++ test/test-0.10.6.js
@@ -0,0 +1,55 @@
+import RNTest from './react-native-testkit/'
+import React from 'react'
+import _ from 'lodash'
+import RNFetchBlob from 'react-native-fetch-blob'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Linking,
+  Platform,
+  Dimensions,
+  BackAndroid,
+  AsyncStorage,
+  Image,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker'
+
+
+const JSONStream = RNFetchBlob.JSONStream
+const fs = RNFetchBlob.fs
+const { Assert, Comparer, Info, prop } = RNTest
+const describe = RNTest.config({
+  group : '0.10.5',
+  run : true,
+  expand : true,
+  timeout : 20000,
+})
+const { TEST_SERVER_URL, TEST_SERVER_URL_SSL, FILENAME, DROPBOX_TOKEN, styles } = prop()
+const dirs = RNFetchBlob.fs.dirs
+let prefix = ((Platform.OS === 'android') ? 'file://' : '')
+let begin = Date.now()
+
+Platform.OS === 'android' &&
+
+describe('Downlaod a file and add to Downlaods App (Android) ', (report, done) => {
+
+  RNFetchBlob
+    .config({ path : dirs.DocumentDir + '/github'+new Date()+'.png' })
+    .fetch('GET', `${TEST_SERVER_URL}/public/github.png`)
+    .then((res) => {
+      console.log(res.path())
+      return RNFetchBlob.android.addCompleteDownload({
+        title : 'test file of RNFB',
+        description : 'desc',
+        mime : 'image/png',
+        path : res.path(),
+        showNotification : true
+      })
+    })
+    .then(() => {
+      done()
+    })
+
+})
