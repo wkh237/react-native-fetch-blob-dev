1d0b12c138480c714badd63f3ff425fc386703ce
Michael Hasenstein <hasenstein@yahoo.com>
2017-08-16 11:02:54 +0200

    test-0.6.0: the only tests in there are for writeFile, readFile, appendFile, and those should be in test-fs.js (copied there)

--- test/test-0.6.0.js
+++ test/test-0.6.0.js
@@ -25,65 +25,71 @@ const  dirs = RNFetchBlob.fs.dirs
 
 let prefix = ((Platform.OS === 'android') ? 'file://' : '')
 
-describe('writeFile and readFile test', (report, done) => {
-  let path = dirs.DocumentDir + '/0.6.0-'+Date.now()+'/writeFileTest'+Date.now()
-  let data = 'hellofrom'+Date.now()
-  fs.writeFile(path, data)
-    .then(() => fs.readFile(path, 'utf8'))
-    .then((actual) => {
-      report(<Assert key="utf8 content should correct" expect={data} actual={actual}/>)
-      data = 'base64'
-      return fs.writeFile(path, RNFetchBlob.base64.encode('base64'), 'base64')
-    })
-    .then(() => fs.readFile(path, 'base64'))
-    .then((actual) => {
-      report(<Assert key="base64 content should correct"
-        expect={RNFetchBlob.base64.decode(RNFetchBlob.base64.encode(data))}
-        actual={RNFetchBlob.base64.decode(actual)}/>)
-      data = 'ascii'
-      return fs.writeFile(path, getASCIIArray('ascii'), 'ascii');
-    })
-    .then(() => fs.readFile(path, 'ascii'))
-    .then((actual) => {
-      console.log(getASCIIArray(data), actual)
-      report(<Assert key="ascii content should correct"
-        expect={getASCIIArray(data)}
-        comparer={Comparer.equalToArray}
-        actual={actual}/>)
-      done()
-    })
-})
 
-describe('append file test', (report, done) => {
-  let path = dirs.DocumentDir + '/append-test'+Date.now()
-  let content = 'test on ' + Date.now()
-  fs.writeFile(path, content, 'utf8')
-    .then(() => fs.appendFile(path, '100', 'utf8', true))
-    .then(() => fs.readFile(path, 'utf8'))
-    .then((data) => {
-      report(
-        <Assert key="utf8 data should be appended"
-          expect={content + '100'}
-          actual={data} />)
-      return fs.appendFile(path, getASCIIArray('200'), 'ascii')
-    })
-    .then(() => fs.readFile(path, 'ascii'))
-    .then((data) => {
-      report(<Assert key="ascii data should be appended"
-        expect={getASCIIArray(content + '100' + '200')}
-        comparer={Comparer.equalToArray}
-        actual={data} />)
-      return fs.appendFile(path, RNFetchBlob.base64.encode('300'), 'base64')
-    })
-    .then(() => fs.readFile(path, 'base64'))
-    .then((data) => {
-      report(<Assert key="base64 data should be appended"
-        expect={content + '100' + '200' + '300'}
-        actual={RNFetchBlob.base64.decode(data)} />)
-        done()
-    })
 
-})
+// MOVED writeFile, readFile, appendFile tests TO ./test-fs.js
+
+
+
+// describe('writeFile and readFile test', (report, done) => {
+//   let path = dirs.DocumentDir + '/0.6.0-'+Date.now()+'/writeFileTest'+Date.now()
+//   let data = 'hellofrom'+Date.now()
+//   fs.writeFile(path, data)
+//     .then(() => fs.readFile(path, 'utf8'))
+//     .then((actual) => {
+//       report(<Assert key="utf8 content should correct" expect={data} actual={actual}/>)
+//       data = 'base64'
+//       return fs.writeFile(path, RNFetchBlob.base64.encode('base64'), 'base64')
+//     })
+//     .then(() => fs.readFile(path, 'base64'))
+//     .then((actual) => {
+//       report(<Assert key="base64 content should correct"
+//         expect={RNFetchBlob.base64.decode(RNFetchBlob.base64.encode(data))}
+//         actual={RNFetchBlob.base64.decode(actual)}/>)
+//       data = 'ascii'
+//       return fs.writeFile(path, getASCIIArray('ascii'), 'ascii');
+//     })
+//     .then(() => fs.readFile(path, 'ascii'))
+//     .then((actual) => {
+//       console.log(getASCIIArray(data), actual)
+//       report(<Assert key="ascii content should correct"
+//         expect={getASCIIArray(data)}
+//         comparer={Comparer.equalToArray}
+//         actual={actual}/>)
+//       done()
+//     })
+// })
+//
+// describe('append file test', (report, done) => {
+//   let path = dirs.DocumentDir + '/append-test'+Date.now()
+//   let content = 'test on ' + Date.now()
+//   fs.writeFile(path, content, 'utf8')
+//     .then(() => fs.appendFile(path, '100', 'utf8', true))
+//     .then(() => fs.readFile(path, 'utf8'))
+//     .then((data) => {
+//       report(
+//         <Assert key="utf8 data should be appended"
+//           expect={content + '100'}
+//           actual={data} />)
+//       return fs.appendFile(path, getASCIIArray('200'), 'ascii')
+//     })
+//     .then(() => fs.readFile(path, 'ascii'))
+//     .then((data) => {
+//       report(<Assert key="ascii data should be appended"
+//         expect={getASCIIArray(content + '100' + '200')}
+//         comparer={Comparer.equalToArray}
+//         actual={data} />)
+//       return fs.appendFile(path, RNFetchBlob.base64.encode('300'), 'base64')
+//     })
+//     .then(() => fs.readFile(path, 'base64'))
+//     .then((data) => {
+//       report(<Assert key="base64 data should be appended"
+//         expect={content + '100' + '200' + '300'}
+//         actual={RNFetchBlob.base64.decode(data)} />)
+//         done()
+//     })
+//
+// })
 
 function getASCIIArray(str) {
   let r = []
